60db8dfb9d1da8909f98c22bfd375da5236b540c
Victor Martins <vitorchiesa74@gmail.com>
2022-07-01 21:14:00 -0300

    Add NavBar, Footer, Routers, About

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { PrimaryPageComponent } from './pages/primary-page/primary-page.component';
+import { DonationsComponent } from './pages/donations/donations.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: PrimaryPageComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,11 +11,21 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AboutComponent } from './pages/about/about.component';
+import { DonationsComponent } from './pages/donations/donations.component';
+import { MatTableModule } from '@angular/material/table';
+import { PrimaryPageComponent } from './pages/primary-page/primary-page.component';
+import { MenuNavComponent } from './components/menu-nav/menu-nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    AboutComponent,
+    DonationsComponent,
+    PrimaryPageComponent,
+    MenuNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +37,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
